d4b963437f641dd2a83ddfdb143843f7feb492cd
RispaJoseph <rizpahjosph@gmail.com>
2023-11-02 23:34:32 +0530

    UI design

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from core.views import index
+# from django.urls import path
+# from core.views import index
 
-app_name = "core"
+# # app_name = "core"
 
-urlpatterns = [
-    path("core/", index)
-]
+# # urlpatterns = [
+# #     path("core/", index)
+# # ]
+
+# # from django.contrib import admin
+# # from django.urls import path, include
+# # from home import views
+
+# urlpatterns = [
+#     path("core/",views.index,name='core')
+# ]
